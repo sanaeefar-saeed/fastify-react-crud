da8a7df32d82d6c491bfe08c6326ac18f124a587
novonimo <nimamohamadian89@gmail.com>
2019-04-18 08:02:08 +0430

    addd redux-persist

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Provider} from 'react-redux'
-import store from '../store/store'
+import {store, persistor} from '../store/store'
 import FastifyApp from './FastifyApp'
+import {PersistGate} from 'redux-persist/integration/react'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <FastifyApp/>
+      <PersistGate loading={null} persistor={persistor}>
+        <FastifyApp/>
+      </PersistGate>
     </Provider>
   )
 };
